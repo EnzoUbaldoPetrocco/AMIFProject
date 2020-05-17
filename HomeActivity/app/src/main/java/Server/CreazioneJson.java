28970a70ccfa5e35e47cab939e28225de83e9f4a
Alessio Roda <alessioroda98@gmail.com>
2020-05-17 23:25:14 +0200

    Versione alternativa per creazione oggetti json (non ho eliminato la precedente)

--- HomeActivity/app/src/main/java/Server/CreazioneJson.java
+++ HomeActivity/app/src/main/java/Server/CreazioneJson.java
@@ -1,10 +1,14 @@
 package Server;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class CreazioneJson {
 
     private static String data=null;
 
     //Json per la post dell'account da creare in Signin
+
     public static String signinJson(String username, String password)
     {
         data= "{"+
@@ -12,6 +16,32 @@ public class CreazioneJson {
                 "\"password\"" + "\""+password+"\""+
                 "}";
         return data;
+
     }
+
+    //Versione alternativa per la creazione di oggetti Json
+    //Nel pimo array vanno scritti i nomi dei campi da creare per il
+    /*
+    public static JSONObject convert2Json(String[] nomi, String...strings)
+    {
+        JSONObject jsonObject=new JSONObject();
+
+        try {
+            for (int i=0; i<strings.length; i++)
+            {
+                jsonObject.put(nomi[i], strings[i]);
+            }
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return  jsonObject;
+    }
+
+     */
+
+
+
 }
 
