4fede771a530ed4b8dbe84121db970bcc5cdb8f8
Enzo Petrocco <enzopetrocco@hotmail.it>
2020-07-09 14:49:31 +0200

    Creazione classe WSApi da finire di implementare

--- HomeActivity/app/src/main/java/Server/Server.java
+++ HomeActivity/app/src/main/java/Server/Server.java
@@ -3,6 +3,7 @@ package Server;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -15,6 +16,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.WebSocket;
+import okhttp3.WebSocketListener;
+import okio.ByteString;
 
 
 public class Server {
@@ -188,6 +195,36 @@ public class Server {
                     }
                 });
     }
+
+    public class WSApi extends WebSocketListener {
+        private static final int NORMAL_CLOSURE_STATUS = 1000;
+        @Override
+        public void onOpen(WebSocket webSocket, Response response) {
+            webSocket.send("Hello, it's SSaurel !");
+            webSocket.send("What's up ?");
+            webSocket.send(ByteString.decodeHex("deadbeef"));
+            webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye !");
+        }
+        @Override
+        public void onMessage(WebSocket webSocket, String text) {
+
+        }
+        @Override
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+
+        }
+        @Override
+        public void onClosing(WebSocket webSocket, int code, String reason) {
+            webSocket.close(NORMAL_CLOSURE_STATUS, null);
+
+        }
+        @Override
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+
+        }
+    }
+
+
 
 
 }
